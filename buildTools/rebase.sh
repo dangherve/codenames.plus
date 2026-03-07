d215e14adc2cfd305ec123cad4b768425fc22351
dangherve <dangherve@gmail.com>
2026-03-07 01:24:44 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/fxaa/codenames.plus.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/codenames.plus.git
+git push --force --set-upstream origin master
